c83b0e6a38ad030b7b3056488782cace6dbcba99
Shane Shifflett <shifflett.shane@gmail.com>
2012-08-27 13:35:46 -0700

    copy changes

--- annotations.js
+++ annotations.js
@@ -7,9 +7,9 @@ var annotations = [
         'textSegments': {
             'currently': 'Occasional commercial film studio',
             'cause': 'Repair of ships exposed to radiation from atomic bomb tests. An optical shop handled radioactive materials.',
-            'potentialContaminents': 'Ra-226 and Th-232',
+            'potentialContaminents': 'Radium-226 and thorium-232',
             'recommendedSteps': 'Scoping floor spaces and sewer drains for the optical shop.',
-            'contaminationLikelihood': 'likely (in drains)'
+            'contaminationLikelihood': 'Likely (in drains)'
         }
     },
     {
@@ -20,9 +20,9 @@ var annotations = [
         'textSegments': {
             'currently': 'Storage and office building for cleanup of contaminated site.',
             'cause': 'Contaminated soil from Site 12 placed in the yard surrounding the building.',
-            'potentialContaminents': 'Ra-226',
+            'potentialContaminents': 'Radium-226',
             'recommendedSteps': 'Do a survey of structures and yard.',
-            'contaminationLikelihood': 'unlikely', 
+            'contaminationLikelihood': 'Unlikely', 
         }
     },
     {
@@ -32,36 +32,36 @@ var annotations = [
         'name': 'USS Pandemonium Site II (NE), Area 6',
         'textSegments': {
             'currently': 'NA',
-            'cause': 'Unlicensed radioactive sources used to check instruments, procedures for disposal of radioactive sources may not have been followed. Radioactive decontamination training performed here.',
-            'potentialContaminents': 'Ra-226, Cs-137',
-            'recommendedSteps': 'A scoping survey of the concrete holding tanks, structures, and ground surface in the former training area.',
-            'contaminationLikelihood': 'unlikely', 
+            'cause': 'Unlicensed radioactive sources used to check instruments; procedures for disposal of radioactive sources may not have been followed. Radioactive decontamination training performed here.',
+            'potentialContaminents': 'Radium-226, cesium-137',
+            'recommendedSteps': 'A scoping survey of the concrete holding tanks, structures and ground surface in the former training area.',
+            'contaminationLikelihood': 'Unlikely', 
         }
     },
     {
         'type': 'point',
         'id': '-1',
         'latlng': [37.830641,-122.370004],
-        'name': 'Former Salvage Yard Site outside Building 327, Area 6',
+        'name': 'Former salvage yard site outside Building 327, Area 6',
         'textSegments': {
-            'currently': 'Sewage Treatment Plant',
+            'currently': 'Sewage treatment plant',
             'cause': 'Salvage yards handle industrial debris, which are often radiologically contaminated.',
-            'potentialContaminents': 'Ra-226',
+            'potentialContaminents': 'Radium-226',
             'recommendedSteps': 'Complete scoping survey of ground surface in sewage treatment plant',
-            'contaminationLikelihood': 'unlikely', 
+            'contaminationLikelihood': 'Unlikely', 
         }
     },
     {
         'type': 'point',
         'latlng': [37.830259903513955,-122.37108707427979],
         'id': '6',
-        'name': 'Waste and Clean Soil Stockpile/Loading and decontamination Site (also known as the site 6 RCA), Area 6',
+        'name': 'Waste and clean soil stockpile/loading and decontamination site (also known as the site 6 RCA), Area 6',
         'textSegments': {
-            'currently': 'Open Area used for remediation of the site',
+            'currently': 'Open area used for remediation of the site',
             'cause': 'Contaminated soil associated with Site 12 was moved here and repacked for shipment.',
-            'potentialContaminents': 'Ra-226',
+            'potentialContaminents': 'Radium-226',
             'recommendedSteps': 'Complete survey of the ground surface in the site.',
-            'contaminationLikelihood': 'unlikely', 
+            'contaminationLikelihood': 'Unlikely', 
         }
     },
     {
@@ -70,24 +70,11 @@ var annotations = [
         'latlng': [37.824739,-122.376345],
         'name': 'USS Pandemonium Site I (NW), Area 8',
         'textSegments': {
-            'currently': 'Multi-family housing',
+            'currently': 'Multifamily housing',
             'cause': 'Radiological handling in the salvage and waste disposal area.  Nuclear, Biological, and Chemical Warfare School may have conducted unlicensed checks of radioactive sources or failed to comply with procedures.',
-            'potentialContaminents': 'Ra-226 and Cs-137',
-            'recommendedSteps': 'Complete scoping survey of former holding tanks, structures, and ground surface at site. Gamma ray walkover survey of roads not previously tested.',
-            'contaminationLikelihood': 'unlikely', 
-        }
-    },
-    {
-        'type': 'point',
-        'latlng': [37.82571771605949,-122.37325429916383],
-        'id': '30',
-        'name': 'Former storage areas and sites 30 and 31, Area 8',
-        'textSegments': {
-            'currently': 'Elementary school, daycare center, storage area, parking lot',
-            'cause': 'Salvage yards that handle industrial debris tend to be radiologically contaminated. Higher levels of Ra-226 detected during excavation, possibly from a deteriorated metal gauge.',
-            'potentialContaminents': 'Ra-226',
-            'recommendedSteps': 'Soil remediation done in 2010.  Phase II remediation for Site 31 ongoing. Survey after remediation.',
-            'contaminationLikelihood': 'likely', 
+            'potentialContaminents': 'Radium-226 and cesium-137',
+            'recommendedSteps': 'Complete scoping survey of former holding tanks, structures and ground surface at site. Gamma ray walkover survey of roads not previously tested.',
+            'contaminationLikelihood': 'Unlikely', 
         }
     },
     {
@@ -96,11 +83,11 @@ var annotations = [
         'id': '31',
         'name': 'Former storage areas and sites 30 and 31, Area 8',
         'textSegments': {
-            'currently': 'Elementary school, daycare center, storage area, parking lot',
-            'cause': 'Salvage yards that handle industrial debris tend to be radiologically contaminated. Higher levels of Ra-226 detected during excavation, possibly from a deteriorated metal gauge.',
-            'potentialContaminents': 'Ra-226',
+            'currently': 'Elementary school, day care center, storage area, parking lot',
+            'cause': 'Salvage yards that handle industrial debris tend to be radiologically contaminated. Higher levels of radium-226 detected during excavation, possibly from a deteriorated metal gauge.',
+            'potentialContaminents': 'Radium-226',
             'recommendedSteps': 'Soil remediation done in 2010.  Phase II remediation for Site 31 ongoing. Survey after remediation.',
-            'contaminationLikelihood': 'likely', 
+            'contaminationLikelihood': 'Likely', 
         }
     },
     {
@@ -109,24 +96,24 @@ var annotations = [
         'latlng': [37.824582125509764,-122.36582994461058],
         'name': 'Building 342, Area 4',
         'textSegments': {
-            'currently': 'Storage Area',
-            'cause': 'Housed the Radiation Detection, Indication And Computation Maintenance school, and instrument calibration and instruction required periodic leaking of radioactive materials. Counting room for testing contamination.',
-            'potentialContaminents': 'Ra-226, Cs-137',
+            'currently': 'Storage area',
+            'cause': 'Housed the Radiation Detection, Indication And Computation Maintenance school, where instrument calibration and instruction required periodic leaking of radioactive materials, and counting room for testing contamination.',
+            'potentialContaminents': 'Radium-226, Cesium-137',
             'recommendedSteps': 'Scope and survey of structure and sanitary sewer line.',
-            'contaminationLikelihood': 'unlikely', 
+            'contaminationLikelihood': 'Unlikely', 
         }
     },
     {
         'type': 'point',
         'latlng': [37.82554822604209,-122.36535787582399],
         'id': '3',
-        'name': 'Former Salvage Yard (Lot 69), Area 4',
+        'name': 'Former salvage yard (Lot 69), Area 4',
         'textSegments': {
             'currently': 'Storage Area',
             'cause': '1962-1968 salvage yard. Separately listed as a hazardous waste area by 1996.',
-            'potentialContaminents': 'Ra-226',
+            'potentialContaminents': 'Radium-226',
             'recommendedSteps': 'Scoping survey of the ground surface',
-            'contaminationLikelihood': 'unlikely', 
+            'contaminationLikelihood': 'Unlikely', 
         }
     },
     {
@@ -135,9 +122,9 @@ var annotations = [
         'name': 'Site 12',
         'textSegments': {
             'currently': '900 two-story residential housing units',
-            'potentialContaminents': 'Ra-226',
-            'cause': 'Excavated 581 trenches, low level radiation determined to be natural. Follow-ups by Navy say they are naturally occurring Th-232 and Ra-226.',
-            'recommendedSteps': 'Gamma walk-over surveys',
+            'potentialContaminents': 'Radium-226',
+            'cause': 'Excavated 581 trenches, low-level radiation determined to be natural. Follow-ups by Navy say they are naturally occurring thorium-232 and radium-226.',
+            'recommendedSteps': 'Gamma walkover surveys',
             'contaminationLikelihood': 'NA'
         }
     },
@@ -148,9 +135,9 @@ var annotations = [
         'name': 'Building 233, Area 2',
         'textSegments': {
             'currently': 'Demolished building (2011)',
-            'cause': '1950 Radium sulfate spill was cleaned up at the time, but a 2007 Survey found piping could be impacted.',
+            'cause': '1950 Radium sulfate spill was cleaned up at the time, but a 2007 survey found piping could be impacted.',
             'recommendedSteps': 'Surveys to make sure cleanup meets current standards, remediation of sewer system and surrounding areas.',
-            'potentialContaminents': 'Ra-226',
+            'potentialContaminents': 'Radium-226',
             'contaminationLikelihood': 'NA',
         }
     },
@@ -161,7 +148,7 @@ var annotations = [
         'name': 'Buildings 343 and 344, Area 4',
         'textSegments': {
             'currently': 'Radioactive contamination cleaned up and disposed of, cleared for use in 2007 and 2008.',
-            'cause': "Two alpha radiation sources discovered and decontaminated in the 70's. A 1988 investigation found it incomplete.",
+            'cause': "Two alpha radiation sources discovered and decontaminated in the 1970s. A 1988 investigation found it incomplete.",
             'recommendedSteps': 'NA',
             'potentialContaminents': 'NA',
             'contaminationLikelihood': 'NA',
